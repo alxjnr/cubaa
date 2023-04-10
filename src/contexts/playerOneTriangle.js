c8aca14048f993ccc3e9752efac6bccaa2783e42
alxjnr <alexrobertjoiner@gmail.com>
2023-04-10 15:36:52 +0100

    Updates

--- src/contexts/playerOneTriangle.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const playerOneTriangleContext = createContext();
--- src/socket.js
+++
@@ -1,6 +0,0 @@
-import { io } from "socket.io-client";
-
-// const URL =
-//   process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
-
-export const socket = io("http://54.38.78.231:55100");
