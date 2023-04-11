256401ace664e21c5b95ed4bcdaaa72cbf4994cd
alxjnr <alexrobertjoiner@gmail.com>
2023-04-11 14:31:31 +0100

    change socket port

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 // const URL =
 //   process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
 
-export const socket = io("https://api.cubaa.net");
+export const socket = io("https://api.cubaa.net:5001");
