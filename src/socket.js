530c45f0cf164a67e8297572a06bbf22b02f0afc
alxjnr <alexrobertjoiner@gmail.com>
2023-04-11 13:23:02 +0100

    update socket domain

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 // const URL =
 //   process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
 
-export const socket = io("https://54.38.78.231:80");
+export const socket = io("https://api.cubaa.net");
