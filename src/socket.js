b6ad6ab51b48ad823546729813cff10b6d133a92
alxjnr <alexrobertjoiner@gmail.com>
2023-04-11 11:45:22 +0100

    https build

--- src/socket.js
+++ src/socket.js
@@ -3,4 +3,4 @@ import { io } from "socket.io-client";
 // const URL =
 //   process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
 
-export const socket = io("http://54.38.78.231:5000");
+export const socket = io("https://54.38.78.231:5000");
